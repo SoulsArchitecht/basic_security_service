2b655f1cb8bd229679deedcc9c004594a410d782
Serge <sshibkojavadev@gmail.com>
2024-11-11 18:37:49 +0800

    init commit

---
+++ src/test/java/ru/sshibko/bss/BasicSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sshibko.bss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
